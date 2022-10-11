cba545bc5c55d69a2b132fef85bd1bf51a86dea0
Alex Taboada <alexandrotaboada@gmail.com>
2022-10-11 14:28:42 -0300

    refactor

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,8 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+app.get('/', (req: Request, res: Response) => res.send('Express + TypeScript Server') );
 
 app.use('/api', routes)
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+app.listen(port, () => console.log(`⚡️[server]: localhost:${port}`) );
