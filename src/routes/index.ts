758cdbba30646980ac015c6739c028450b34a930
Alex Taboada <alexandrotaboada@gmail.com>
2022-10-11 16:30:31 -0300

    listen to Mercado Pago Notifications

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 const router = Router()
 
 import mercadopago from './mercadopago';
+import IPN from './IPN';
 
 router.use('/mercadopago', mercadopago)
+router.use('/ipn', IPN)
 
 export default router
